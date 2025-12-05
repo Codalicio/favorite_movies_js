e1fcead551f8075d79cf487fce149193fbf7c2f1
Amit Raj <rajamitnine@gmail.com>
2025-12-05 13:55:49 +0530

    Select and add an element

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,8 @@
 const addMovieModalElement = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
 const backdropElement = document.getElementById("backdrop");
+const cancelAddMovieButton =
+  addMovieModalElement.querySelector(".btn--passive");
 
 const toggleBackdrop = () => {
   backdropElement.classList.toggle("visible");
