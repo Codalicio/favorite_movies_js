42e01375f571857bec6ade1d60af8567b050e415
Amit Raj <rajamitnine@gmail.com>
2025-12-05 13:37:06 +0530

    Add a click event listener to the button, and point it to toggleMovieModal function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,2 +1,8 @@
 const addMovieModalElement = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
+
+const toggleMovieModal = () => {
+  addMovieModalElement.classList.toggle("visible");
+};
+
+startAddMovieButton.addEventListener("click", toggleMovieModal);
