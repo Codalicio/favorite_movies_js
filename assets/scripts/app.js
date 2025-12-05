e8cf820232f7c23d5a3458104e5f01b1ebdfec57
Amit Raj <rajamitnine@gmail.com>
2025-12-05 13:45:08 +0530

    Select and add an element, also define a function toggleBackdrop()

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,14 @@
 const addMovieModalElement = document.getElementById("add-modal");
 const startAddMovieButton = document.querySelector("header button");
+const backdropElement = document.getElementById("backdrop");
+
+const toggleBackdrop = () => {
+  backdropElement.classList.toggle("visible");
+};
 
 const toggleMovieModal = () => {
   addMovieModalElement.classList.toggle("visible");
+  toggleBackdrop();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
